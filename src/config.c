7f6ec3dd8446696cbfaf9de4117feaafacdc0c3e
Anthon Krotov <krotov.anthon@yandex.ru>
2025-12-02 21:13:57 +1000

    config parse yaml -- inner (network, detection)

--- src/config.c
+++ src/config.c
@@ -63,11 +63,92 @@ int config_load(const char *path, passible_config *out) {
                               "ignore_public_dns")) {
               out->network.ignore_public_dns =
                   atoi((char *)network_value_node->data.scalar.value);
+            } else if (strcmp((char *)network_key_node->data.scalar.value,
+                              (char *)network_key_node->data.scalar.value) ==
+                           0 &&
+                       network_value_node->type == YAML_SCALAR_NODE) {
+              out->network.ignore_destions_len =
+                  network_value_node->data.sequence.items.top -
+                  network_value_node->data.sequence.items.start;
+              out->network.ignore_destinations =
+                  malloc(out->network.ignore_destions_len * sizeof(char *));
+              size_t index = 0;
+              for (yaml_node_item_t *item =
+                       network_value_node->data.sequence.items.start;
+                   item < network_value_node->data.sequence.items.top;
+                   item++, index++) {
+                yaml_node_t *arr_item_node =
+                    yaml_document_get_node(&document, *item);
+                if (arr_item_node->type == YAML_SCALAR_NODE) {
+                  out->network.ignore_destinations[index] =
+                      strdup((char *)arr_item_node->data.scalar.value);
+                }
+              }
             }
           }
         }
       } else if (strcmp((char *)key_node->data.scalar.value, "detection") &&
                  value_node->type == YAML_MAPPING_NODE) {
+        for (yaml_node_pair_t *detection_pair =
+                 key_node->data.mapping.pairs.start;
+             detection_pair < key_node->data.mapping.pairs.top;
+             detection_pair++) {
+          yaml_node_t *detection_key_node =
+              yaml_document_get_node(&document, detection_pair->key);
+          yaml_node_t *detection_value_node =
+              yaml_document_get_node(&document, detection_pair->value);
+          if (detection_key_node->type == YAML_SCALAR_NODE) {
+            if (strcmp((char *)detection_key_node->data.scalar.value,
+                       "trusted_processes") == 0 &&
+                detection_value_node->type == YAML_SCALAR_NODE) {
+              out->detection.trusted_processes_len =
+                  detection_value_node->data.sequence.items.top -
+                  detection_value_node->data.sequence.items.start;
+              out->detection.trusted_processes =
+                  malloc(out->detection.trusted_processes_len * sizeof(char *));
+              size_t index = 0;
+              for (yaml_node_item_t *item =
+                       detection_value_node->data.sequence.items.start;
+                   item < detection_value_node->data.sequence.items.top;
+                   item++, index++) {
+                yaml_node_t *arr_item_node =
+                    yaml_document_get_node(&document, *item);
+                if (arr_item_node->type == YAML_SCALAR_NODE) {
+                  out->detection.trusted_processes[index] =
+                      strdup((char *)arr_item_node->data.scalar.value);
+                }
+              }
+            } else if (strcmp((char *)detection_key_node->data.scalar.value,
+                              "suspicious_ports") == 0 &&
+                       detection_value_node->type == YAML_SCALAR_NODE) {
+              out->detection.suspicious_ports_len =
+                  detection_value_node->data.sequence.items.top -
+                  detection_value_node->data.sequence.items.start;
+              out->detection.suspicious_ports =
+                  malloc(out->detection.suspicious_ports_len * sizeof(char *));
+              size_t index = 0;
+              for (yaml_node_item_t *item =
+                       detection_value_node->data.sequence.items.start;
+                   item < detection_value_node->data.sequence.items.top;
+                   item++, index++) {
+                yaml_node_t *arr_item_node =
+                    yaml_document_get_node(&document, *item);
+                if (arr_item_node->type == YAML_SCALAR_NODE) {
+                  out->detection.suspicious_ports[index] =
+                      atoi((char *)arr_item_node->data.scalar.value);
+                }
+              }
+            } else if (strcmp((char *)detection_key_node->data.scalar.value,
+                              "min_heartbeat_interval_sec") == 0) {
+              out->detection.min_heartbeat_interval_sec =
+                  atoi((char *)detection_value_node->data.scalar.value);
+            } else if (strcmp((char *)detection_key_node->data.scalar.value,
+                              "enable_entropy_check") == 0) {
+              out->detection.enable_entropy_check =
+                  atoi((char *)detection_value_node->data.scalar.value);
+            }
+          }
+        }
       }
     }
   }
